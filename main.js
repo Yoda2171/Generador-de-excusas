cd3ed6f0d99cb5856e31b986917fd5f0b0ec16e4
Yoda2171 <popotaya7@gmail.com>
2021-01-04 13:14:14 -0300

    generador listo

---
+++ main.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+document.querySelector("#excuse").innerHTML = generador();
+};
+
+function generador() {
+    
+    let who = ['the dog', 'my granma', 'his turtle', 'my bird'];
+    let what = ['eat', 'pissed', 'crushed', 'broked'];
+    let when = ['before the class', 'right in time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+    let whoindx = Math.floor(Math.random() * who.length)
+    let whatindx = Math.floor(Math.random() * what.length)
+    let whenindx = Math.floor(Math.random() * when.length)
+
+    return who[whoindx] + " " + what[whatindx] + " " + when[whenindx];
+};
+
